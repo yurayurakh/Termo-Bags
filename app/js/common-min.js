c361c4dccdece5a63a09fff1d175d3b7cdf8e856
George <yurayurakh@mail.ru>
2017-09-11 00:30:39 +0300

    block3

--- app/js/common-min.js
+++ app/js/common-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".hides-answer").hide(),$(".question__item").click(function(){$(this).parent().toggleClass("substrate-question"),$(this).children(".question").toggleClass("question-open").next().slideToggle(300)})});
+$(document).ready(function(){$(".hides-answer").hide(),$(".question__item").click(function(){$(this).parent().toggleClass("substrate-question"),$(this).children(".question").toggleClass("question-open").next().slideToggle(300)}),$(".parameter__item_name").click(function(){$(this).toggleClass("parameter-active"),$(this).next().slideToggle(300)})});
--- app/js/common.js
+++ app/js/common.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     $(".question__item").click(function () {
         $(this).parent().toggleClass("substrate-question");
         $(this).children(".question").toggleClass("question-open").next().slideToggle(300);
+    });
+    $(".parameter__item_name").click(function () {
+        $(this).toggleClass("parameter-active");
+        $(this).next().slideToggle(300);
     })
 });
